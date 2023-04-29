8e34755e758d9c32287058662c3782502c8d26a6
al-martyn1 <amart@mail.ru>
2023-04-29 19:26:03 +0300

    Переделал с использованием обновлённых библиотек

--- src/umba-sort-headers.cpp
+++ src/umba-sort-headers.cpp
@@ -31,7 +31,7 @@
 #include "marty_cpp/src_normalization.h"
 
 #include "encoding/encoding.h"
-#include "app_utils.h"
+#include "umba/cli_tool_helpers.h"
 
 
 #if defined(WIN32) || defined(_WIN32)
@@ -150,126 +150,69 @@ int main(int argc, char* argv[])
     // }
 
 
-    if (inputFilename.empty())
+    try
     {
-        inputFilename = "STDIN";
-    }
+        umba::cli_tool_helpers::IoFileType inputFileType  = umba::cli_tool_helpers::IoFileType::nameEmpty;
+        umba::cli_tool_helpers::IoFileType outputFileType = umba::cli_tool_helpers::IoFileType::nameEmpty;
+        adjustInputOutputFilenames(inputFilename, inputFileType, outputFilename, outputFileType);
 
+       
+        bool utfSource = false;
+        bool checkBom  = true;
+        bool fromFile  = true;
 
-    std::string checkMsg;
+        std::string srcData = umba::cli_tool_helpers::readInput( inputFilename , inputFileType
+                            , encoding::ToUtf8(), checkBom, fromFile, utfSource //, outputLinefeed
+                            );
 
-    app_utils::IoFileType inputFileType = app_utils::detectFilenameType(inputFilename, true /* input file */);
-    if (!app_utils::checkIoFileType(inputFileType, checkMsg, true /* input file */))
-    {
-        LOG_ERR_OPT << checkMsg << "\n";
-        return 1;
-    }
+        //  
+        // // Есть ли данные на входе, нет их - это не наша проблема - процессим пустой текст в нормальном режиме
 
-    outputFilename = app_utils::adjustOutputFilename(outputFilename, inputFilename, inputFileType);
-    app_utils::IoFileType outputFileType = app_utils::detectFilenameType(outputFilename, false /* not input file */);
-    if (!app_utils::checkIoFileType(outputFileType, checkMsg, false /* not input file */))
-    {
-        LOG_ERR_OPT << checkMsg << "\n";
-        return 1;
-    }
-
-
-    bool utfSource = false;
-    bool checkBom  = true;
-    bool fromFile  = true;
-    std::string srcData;
-    std::string bomData;
-
-#if defined(HAS_CLIPBOARD_SUPPORT)
-    if (inputFileType==app_utils::IoFileType::clipboard)
-    {
-        checkBom = false;
-        fromFile = false;
-        if (!app_utils::getClipboardTextHelper(srcData, &utfSource))
+        std::string bomData = umba::cli_tool_helpers::stripTheBom(srcData, checkBom, encoding::BomStripper());
+       
+        //------------------------------
+       
+        // Do job itself
+       
+        ELinefeedType detectedSrcLinefeed = ELinefeedType::crlf;
+       
+        std::string lfNormalizedText = marty_cpp::normalizeCrLfToLf(srcData, &detectedSrcLinefeed);
+       
+        if (outputLinefeed==ELinefeedType::unknown || outputLinefeed==ELinefeedType::detect)
         {
-            LOG_ERR_OPT << "failed to get clipboard text\n";
-            return 1;
+            outputLinefeed = detectedSrcLinefeed;
         }
+
         #if defined(WIN32) || defined(_WIN32)
-        outputLinefeed = ELinefeedType::crlf;
+        if (outputFileType==umba::cli_tool_helpers::IoFileType::clipboard)
+        {
+            outputLinefeed = ELinefeedType::crlf;
+        }
         #endif
-    }
-    else
-#endif
-    if (!app_utils::readFileHelper(inputFileType, inputFilename, srcData))
+       
+       
+        std::vector<std::string> textLines = marty_cpp::splitToLinesSimple( lfNormalizedText
+                                                                          , true // addEmptyLineAfterLastLf
+                                                                          , '\n' // lfChar
+                                                                          );
+       
+        std::vector<std::string> sortedLines = marty_cpp::sortIncludes(textLines, sortIncludeOptions);
+       
+        std::string resultText = marty_cpp::mergeLines(sortedLines, outputLinefeed, false /* addTrailingNewLine */);
+       
+        //------------------------------
+
+        umba::cli_tool_helpers::writeOutput( outputFilename, outputFileType
+                                           , encoding::ToUtf8(), encoding::FromUtf8()
+                                           , resultText, bomData
+                                           , fromFile, utfSource, bOverwrite
+                                           );
+
+    } // try
+    catch(const std::runtime_error &e)
     {
-        LOG_ERR_OPT << "failed to read input file '" << inputFilename << "'\n";
-        return 1;
+        LOG_ERR_OPT << e.what() << "\n";
     }
-
-
-    // Есть ли данные на входе, нет их - это не наша проблема - процессим пустой текст в нормальном режиме
-
-    if (checkBom)
-    {
-        bomData = encoding::getEncodingsApi()->stripTheBom(srcData); // app_utils::stripBom(srcData);
-    }
-
-    if (!bomData.empty() && bomData.size()!=3)
-    {
-        // https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D1%80%D0%BA%D0%B5%D1%80_%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%B1%D0%B0%D0%B9%D1%82%D0%BE%D0%B2
-        LOG_ERR_OPT << "unsupported input file encoding, found BOM but not UTF-8\n";
-        return 1;
-    }
-
-
-    //------------------------------
-
-    // Do job itself
-
-    ELinefeedType detectedSrcLinefeed = ELinefeedType::crlf;
-
-    std::string lfNormalizedText = marty_cpp::normalizeCrLfToLf(srcData, &detectedSrcLinefeed);
-
-    if (outputLinefeed==ELinefeedType::unknown || outputLinefeed==ELinefeedType::detect)
-    {
-        outputLinefeed = detectedSrcLinefeed;
-    }
-
-
-    std::vector<std::string> textLines = marty_cpp::splitToLinesSimple( lfNormalizedText
-                                                                      , true // addEmptyLineAfterLastLf
-                                                                      , '\n' // lfChar
-                                                                      );
-
-    std::vector<std::string> sortedLines = marty_cpp::sortIncludes(textLines, sortIncludeOptions);
-
-    std::string resultText = marty_cpp::mergeLines(sortedLines, outputLinefeed, false /* addTrailingNewLine */);
-
-    //------------------------------
-
-
-#if defined(HAS_CLIPBOARD_SUPPORT)
-    if (outputFileType==app_utils::IoFileType::clipboard)
-    {
-        if (fromFile)
-        {
-            // Вход был прочитан из файла, хз какая кодировка
-            // Но кодировка не поменялась в процессе сортировки, просто строки местами поменялись
-            // Детектим кодировку и конвертим в UTF-8, присунув исходный BOM, если был
-            resultText = encoding::toUtf8(bomData+resultText);
-            utfSource  = true;
-        }
-
-        if (!app_utils::setClipboardTextHelper(resultText, utfSource))
-        {
-            LOG_ERR_OPT << "failed to set clipboard text\n";
-            return 1;
-        }
-    }
-    else
-#endif
-    if (!app_utils::writeFileHelper(outputFileType, outputFilename, resultText, bOverwrite))
-    {
-        LOG_ERR_OPT << "failed to write output file '" << outputFilename << "'\n";
-        return 1;
-    }
-
 
     return 0;
 }
